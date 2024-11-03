3aa303c6873afa1b53b029a143a7288fc05e2e8c
unknown <abdulrahman.g.alfahad@gmail.com>
2024-11-03 10:44:02 +0300

    Completed Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static double convertUSD(double amount) {
+        return amount * 3.25;
+    }
+
+    public static double convertUDStoKWD(double amount) {
+        return amount / 3.25;
+    }
+
+    public static double convertEURtoKWD(double amount) {
+        return amount * 3;
+    }
+
     public static void main(String[] args) {
-        
+        double amountKWD = 86;
+        double amountEUR = 105;
+
+        double amountUSD = convertUSD(amountKWD);
+
+
+        System.out.println(amountKWD + " KWD = " + amountUSD + " USD");
+        System.out.println(convertUDStoKWD(amountUSD) + " KWD = " + amountUSD + " USD");
+        System.out.println(amountEUR + " EUR = " + convertEURtoKWD(amountEUR) + " KWD");
+
+
     }
 }
